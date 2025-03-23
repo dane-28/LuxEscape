f56430b9268b215da8c0a13bec6ded3ec1bada30
dane-28 <daniellesapico@gmail.com>
2025-03-23 16:40:57 +0800

    Add initial TypeScript definitions and configuration files for various modules

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const app = express();
-const path = require('path');
  
-// Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
- 
+// Define a basic route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.send('Hello, Express Server!');
 });
  
+// Start the server
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000'); // Fixed this line
+    console.log('Server is running on http://localhost:3000');
 });
